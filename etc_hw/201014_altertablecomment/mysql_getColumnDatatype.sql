2392c4dc28d1b477070fbf3a748f4d8d595ad4f9
201411096 <korea5781@gmail.com>
2020-10-19 12:28:56 +0900

    201019_02 mysql information_schema tableList column metadata

--- etc_hw/201014_altertablecomment/mysql_getColumnDatatype.sql
+++ etc_hw/201014_altertablecomment/mysql_getColumnDatatype.sql
@@ -1,23 +1,28 @@
--- 사용방법 (INFORMATION_SCHEMA를 이용한 ..)
+-- 사용방법 (INFORMATION_SCHEMA를 이용한 .. 컴럼들의 정보들을 가져오기)
 -- SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
 -- WHERE table_schema = 'your_db_name ...'
 -- AND TABLE_NAME = 'your_table_name ...';
 
--- 사용예시1 (INFORMATION_SCHEMA를 이용한 ..)
+-- 사용예시1 (INFORMATION_SCHEMA를 이용한 .. 컬럼들의 이름과 데이터 타입 정보 가져오기)
 -- SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
 -- WHERE table_schema = 'test'
 -- AND TABLE_NAME = 'test';
 
--- 사용예시2 (INFORMATION_SCHEMA를 이용한 ..)
+-- 사용예시2 (INFORMATION_SCHEMA를 이용한 .. 컬럼의 정보를 가져오기)
 -- SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS
 -- WHERE table_schema = 'test'
 -- AND TABLE_NAME = 'test'
 -- AND COLUMN_NAME = 'id';
 
--- 사용예시2 (INFORMATION_SCHEMA를 이용한 ..)
+-- 사용예시2 (INFORMATION_SCHEMA를 이용한 .. 테이블의 컬럼 정보들 가져오기)
 -- SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 -- WHERE table_schema = 'test'
 -- AND TABLE_NAME = 'test';
+
+--사용방법 (INFORMATION_SCHEMA를 이용한 .. 테이블 리스트들을 가져오기)
+select table_name
+from information_schema.TABLES
+where table_schema = 'your_db_name'
 
 -- 사용방법 (show query ..)
 --show COLUMNS FROM your_tableName FROM your_dbName WHERE FIELD ='your_columnName ...';
