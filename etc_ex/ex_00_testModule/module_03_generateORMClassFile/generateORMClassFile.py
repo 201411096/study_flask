c8074f101a481cb9e03969c1afa3b3cc0964703b
201411096 <korea5781@gmail.com>
2020-10-19 22:26:18 +0900

    201019_03

---
+++ etc_ex/ex_00_testModule/module_03_generateORMClassFile/generateORMClassFile.py
@@ -0,0 +1,165 @@
+from flask import Flask, Response, request, jsonify
+from flask_cors import CORS
+import json
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func, distinct, case, between
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+import pandas as pd
+from sqlalchemy.dialects.mysql import \
+        BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE, \
+        DATETIME, DECIMAL, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, \
+        LONGBLOB, LONGTEXT, MEDIUMBLOB, MEDIUMINT, MEDIUMTEXT, NCHAR, \
+        NUMERIC, NVARCHAR, REAL, SET, SMALLINT, TEXT, TIME, TIMESTAMP, \
+        TINYBLOB, TINYINT, TINYTEXT, VARBINARY, VARCHAR, YEAR
+
+app = Flask(__name__)
+CORS(app)
+
+dbName = "test"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://scott:tiger@192.168.56.1:3306/'+dbName
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['JSON_AS_ASCII'] = False
+db = SQLAlchemy(app)
+
+
+# snakeCase To camelCase
+def to_camel_case(snake_str):
+    components = snake_str.split('_')
+    # We capitalize the first letter of each component except the first one
+    # with the 'title' method and join them together.
+    return components[0] + ''.join(x.title() for x in components[1:])
+
+def makeTempTableName(tableName):
+    # tempTableName = tableName[3].upper() + tableName[4:] #appspeed 사용시 ..
+    tempTableName = tableName[0].upper() + tableName[1:]
+    return tempTableName
+
+def appendImportStringToFileContent(fileContent):
+    fileContent += """
+    """
+    return fileContent
+
+def generateFile(tableName, tableComment):
+    columnArray = []
+    fileContent = """"""
+
+    fileContent = appendImportStringToFileContent(fileContent)
+
+    tempTableName = makeTempTableName(tableName)
+
+    fileContent += 'class '+ to_camel_case(tempTableName) +'(Base):\n'
+    fileContent += '\t__tablename__ = ' + "'" + tableName + "' #"+ tableComment +" \n"
+    fileContent += '\t__table_args__ = {\n'
+    fileContent += "\t\t\t'comment' : '"+tableComment+"'\n"
+    fileContent += "\t\t}\n\n"
+
+    tableStructures = db.session.execute('desc ' + tableName)
+    pkFlag = True
+    pkColumn = ''
+    for tableStructure in tableStructures:
+        print(tableStructure)
+        queryForComment = "SELECT column_comment FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = '{0}' AND TABLE_NAME = ".format(dbName)
+        queryForComment += "'"+tableName+"' and COLUMN_NAME = '"+tableStructure[0]+"'"
+        tempComment = db.session.execute(queryForComment)
+        for temp in tempComment:
+            tempComment = str(temp[0])
+        
+        fileContent += "\t" 
+        fileContent += '%-15s \t\t\t\t\t = Column(' % str(tableStructure[0]) 
+        if(tableStructure[1][0:8]=='longtext'):
+            fileContent+= 'TEXT, '
+        elif(tableStructure[1][0:3]=='int'):
+            fileContent+= 'Integer, '
+        elif(tableStructure[1][0:6]=='double'):
+            fileContent+= 'Numeric' + tableStructure[1][6:] +', '
+        elif(tableStructure[1][0:7]=='decimal'):
+            fileContent+= 'Numeric' + tableStructure[1][7:] +', '
+        else:
+            fileContent+= str(tableStructure[1]).upper()+', '
+            
+        if(pkFlag == True):
+            pkFlag = False
+            pkColumn = tableStructure[0]
+            fileContent += 'primary_key=True, '
+        elif(tableStructure[2]=='NO'):
+            fileContent += 'nullable=False, '
+        fileContent += "comment='%s') # %s" % (tempComment, tempComment)
+        fileContent += "\n"
+    fileContent += '\n\tdef __init__(self, %s, **kwargs):\n' % to_camel_case(pkColumn.lower())
+    fileContent +='\t\tself.'+pkColumn+' = '+to_camel_case(pkColumn.lower())+'\n\n'
+    pkFlag = True
+
+    tableStructures2 = db.session.execute('desc ' + tableName)
+    for tableStructure2 in tableStructures2:
+        if(pkFlag==True):
+            pkFlag=False
+            continue
+        fileContent+='\t\t'
+        fileContent+='self.%-30s' % tableStructure2[0]
+        fileContent+="= kwargs.get('%s', None)" % tableStructure2[0]
+        fileContent+='\n'
+
+    fileContent+='\n\tdef __repr__(self):\n'
+    fileContent+='\t\treturn "{'
+
+    pkFlag = True
+    tableStructures3 = db.session.execute('desc ' + tableName)
+    for tableStructure3 in tableStructures3:
+        if(pkFlag == True):
+            pkFlag = False
+        else:
+            fileContent+=', \\\n\t\t'
+
+        fileContent+="'"+tableStructure3[0]+"' : %s"
+        columnArray.append(tableStructure3[0])
+    fileContent+='}" % (\n'
+
+    tableStructures4 = db.session.execute('desc ' + tableName)
+    for tableStructure4 in tableStructures4:
+        fileContent +='\t\t\t\t\tself.'+tableStructure4[0]
+        if(columnArray[len(columnArray)-1] == tableStructure4[0]):
+            continue
+        else:
+            fileContent +=',\n'
+
+    fileContent+= ')'
+
+    with open(to_camel_case(tempTableName)+'.py', 'w', encoding='utf-8') as f:
+        f.write(fileContent)
+
+    return Response(
+        response = json.dumps({'test':'test'}),
+        status = 200,
+        mimetype="application/json"
+    )
+
+def getTableList():
+    print('check in getTableList ...')
+    query = "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '{0}'".format(dbName)
+    result = db.session.execute(query)
+    return result
+
+@app.route('/generateFile')
+def insertColumnComment():
+    tableList = getTableList()
+    commentList = []
+    if(len(commentList)==0):
+        for i in range(len(getTableList().scalar())): # resultProxy.scalar() 사용시 rowcount가 됨, 하지만 result object가 closed되는 문제가 있어서 result object를 새로 받아옴
+            commentList.append('')
+    
+    for idx, table in enumerate(tableList):
+        if(commentList[idx]==''):
+            commentList[idx]=='your_tableComment'
+        generateFile(table[0], commentList[idx])
+
+    return Response(
+        response = json.dumps({'test':'test'}),
+        status = 200,
+        mimetype="application/json"
+    )
+
+
+if __name__ == '__main__':
+    # app.run(host="192.168.0.51", port="5000")
+    app.run(host="192.168.56.1", port="5000")
