a5be9185749c9bb7acede36cb2b9c40ec2431248
201411096 <korea5781@gmail.com>
2020-11-10 17:20:22 +0900

    201110_05

---
+++ ex_04_flask_sse/ver_02/test_sse.py
@@ -0,0 +1,46 @@
+from flask import Flask, Response, render_template, request
+from flask_cors import CORS
+from flask_sse import sse
+import redis
+import json
+
+app = Flask(__name__)
+CORS(app)
+appRedis = redis.StrictRedis(host='localhost', port=6379, db=0) # redis default port : 6379
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/sample')
+def render_template_sample():
+    return render_template('client.html')
+
+def event_stream():
+    pub = appRedis.pubsub() # Publish/Subscribe object (you can subscribe to channels and listen for messages)
+    pub.subscribe('sse_example_channel')
+    for msg in pub.listen():
+        print('check msg in event_stream() ...', msg)
+        if msg['type'] == 'pmessage':
+            event, data = json.loads(msg['data'])
+            yield u'event: {0}\ndata: {1}\n\n'.format(event, data)
+        else:
+            yield u'data: {0}\n\n'.format(msg['data'])
+
+@app.route('/stream', methods=['GET'])
+def get_pushes():
+    return Response(event_stream(), mimetype="text/event-stream")
+
+@app.route('/post')
+def publish_data():
+    appRedis.publish('sse_example_channel', json.dumps({'data':'bb', 'b':'cc'}))
+    return 'publish_data ...'
+
+@app.route('/post2')
+def publish_data2():
+    appRedis.publish('sse_example_channel2', json.dumps({'data':'aa', 'b':'bb'}))
+    return 'publish_data ...'
+
+if __name__ == '__main__':
+     app.run(host="192.168.0.51", port="5000")
+    #  app.run(host="127.0.0.1", port="5000")
