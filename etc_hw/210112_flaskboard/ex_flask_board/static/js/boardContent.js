46d2c2201674ea7e38ae1057dd1b612e9c0bdf9a
201411096 <korea5781@gmail.com>
2021-01-20 10:48:39 +0900

    210120_01 div height를 컨텐츠 내용에 따라 자동 조절

--- etc_hw/210112_flaskboard/ex_flask_board/static/js/boardContent.js
+++ etc_hw/210112_flaskboard/ex_flask_board/static/js/boardContent.js
@@ -3,6 +3,8 @@ console.log('js 연결 확인');
 boardContentId = document.querySelector('#board_content_id').value;
 console.log('boardContentId : ' + boardContentId);
 
+getCommentList();
+
 document.querySelector('#btn_index').addEventListener('click', function(e){
     location.href='/render/index'
 });
@@ -33,7 +35,34 @@ document.querySelector('#comment_write_btn').addEventListener('click', function(
     });
 });
 
-document.querySelector('#test_btn_commentList').addEventListener('click', function(e){
+//테스트
+// document.querySelector('#test_btn_commentList').addEventListener('click', function(e){
+//     getCommentList();
+// });
+
+function makeCommentList(data){
+    var comment_container = document.querySelector('#comment_container');
+    comment_container.innerHTML = '';
+    // console.log('makeCommentList..');
+    // console.log(data['commentListData']);
+    var listData = data['commentListData'];
+    for(var i=0; i<listData.length ; i++){
+        var div = document.createElement('div');
+        div.classList.toggle('comment_container');
+        var span1 = document.createElement('span');
+        var span2 = document.createElement('span');
+        span1.innerText = listData[i]['member_nickname'];
+        span2.innerText = listData[i]['comment_body'];
+        span1.classList.toggle('comment_col_container_01');
+        span2.classList.toggle('comment_col_container_02');
+        div.append(span1);
+        div.append(span2);
+        comment_container.append(div);
+    }
+}
+
+// 댓글 목록을 가져옴
+function getCommentList(){
     var bid = document.querySelector('#board_content_id').value;
     fetch('/comment/list',{
         method : 'POST',
@@ -45,7 +74,8 @@ document.querySelector('#test_btn_commentList').addEventListener('click', functi
         })
     }).then((res)=>res.json())
     .then((data)=>{
-        console.log('commentList...')
-        console.log(data);
+        // console.log('commentList...')
+        // console.log(data);
+        makeCommentList(data);
     });
-});
+}
