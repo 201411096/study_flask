72c9b740449750a785c90e086e1bdc39a69310f4
201411096 <korea5781@gmail.com>
2021-01-24 23:58:48 +0900

    20124_04

--- etc_ex/ex_24_recursive_query_sequence/test_recursive_sequence_02.sql
+++ etc_ex/ex_24_recursive_query_sequence/test_recursive_sequence_02.sql
@@ -202,47 +202,46 @@ INSERT IGNORE INTO `test_recursive` (`id`, `pid`, `nm`) VALUES
 ####################################################################################################
 ####################################################################################################
 --1 게시판 정렬
-with recursive cte as
-(
-select  board_content_id,
-        board_content_pid,
-        member_id,
-        board_content_title,
-        board_content_body,
-        board_content_regdatetime,
-        board_content_edtdatetime,
-        board_content_deleted,
-        0 AS depth,
-        CAST(LPAD(board_content_id, 10, "0") AS VARCHAR(1000)) as grp,
-        CAST(LPAD(board_content_id, 10, "0") AS VARCHAR(1000)) as lvl
-from    board
-where   board_content_pid = -1 and board_id = :board_id
-union all
-select  r.board_content_id,
-        r.board_content_pid,
-        r.member_id,
-        r.board_content_title,
-        r.board_content_body,
-        r.board_content_regdatetime,
-        r.board_content_edtdatetime,
-        r.board_content_deleted,
-        depth + 1 AS depth,
-        grp AS grp,
-        CONCAT(cte.lvl, "-", LPAD(r.board_content_id, 10, "0")) as lvl
-from    board r
-inner join cte
-        on r.board_content_pid = cte.board_content_id
-)
-SELECT * FROM
-(SELECT * FROM
-(SELECT @ROWNUM:=@ROWNUM+1 AS board_content_num, c.* 
-FROM cte c, 
-(SELECT @ROWNUM:=0) b ORDER BY CAST(board_content_id AS INTEGER)) t 
-) a 
-inner join member
-on a.member_id = member.member_id
-ORDER BY grp DESC, lvl
-LIMIT :startrow, :numberInPage
+	with recursive cte as
+	(
+	select  board_content_id,
+			board_content_pid,
+			member_id,
+			board_content_title,
+			board_content_body,
+			board_content_regdatetime,
+			board_content_edtdatetime,
+			board_content_deleted,
+			0 AS depth,
+			CAST(LPAD(board_content_id, 10, "0") AS VARCHAR(1000)) as grp,
+			CAST(LPAD(board_content_id, 10, "0") AS VARCHAR(1000)) as lvl
+	from    board
+	where   board_content_pid = -1 and board_id = :board_id and board_content_deleted = 'N'
+	union all
+	select  r.board_content_id,
+			r.board_content_pid,
+			r.member_id,
+			r.board_content_title,
+			r.board_content_body,
+			r.board_content_regdatetime,
+			r.board_content_edtdatetime,
+			r.board_content_deleted,
+			depth + 1 AS depth,
+			grp AS grp,
+			CONCAT(cte.lvl, "-", LPAD(r.board_content_id, 10, "0")) as lvl
+	from    board r
+	inner join cte
+			on r.board_content_pid = cte.board_content_id
+	)
+	SELECT * FROM
+    (SELECT @ROWNUM:=@ROWNUM+1 AS board_content_num, c.* 
+    FROM cte c, 
+    (SELECT @ROWNUM:=0) b ORDER BY CAST(board_content_id AS INTEGER)) t 
+    inner join member
+    on t.member_id = member.member_id
+    ORDER BY grp DESC, lvl
+	LIMIT :startrow, :numberInPage
+    ;
 
 --2. 댓글 정렬
 with recursive cte as
