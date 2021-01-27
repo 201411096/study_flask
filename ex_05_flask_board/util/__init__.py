39ee2d91b20aec18121f1f822d621187006754f7
201411096 <korea5781@gmail.com>
2021-01-27 14:00:28 +0900

    210127_02

--- ex_05_flask_board/util/__init__.py
+++ ex_05_flask_board/util/__init__.py
@@ -21,3 +21,45 @@ def authDecorator(func):
             return redirect('/render/login')
         return func(*args, **kwargs)
     return wrapper
+
+
+##### test #####
+from sqlalchemy import exc
+
+def convertStatementToList(statement):
+    result = {}
+    dataList = []
+    rows = statement.all()
+
+    for row in rows:
+        dataList.append(row._asdict())
+    result['data'] = dataList
+    return result
+
+# sql 직접 사용시에..
+def convertStatementToList2(data):
+    result = {}
+    dataList = [{column: value for column, value in rowproxy.items()} for rowproxy in data]
+    result['data'] = dataList
+    return result
+
+def returnAfterAdd(session, dataObject):
+    result = {}
+    try:
+        session.add(dataObject)
+        session.commit()
+        result['code'] = '1'
+    except exc.IntegrityError as e:
+        print(e)
+        session.rollback()
+        result['code'] = '0'
+    return result
+
+def returnAfterUpdate(resultCount):
+    result = {}
+    if resultCount>=1:
+        result['code']=1
+    else:
+        result['code']=0		
+    return result
+##### test #####
