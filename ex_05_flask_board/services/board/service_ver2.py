fab44267fa7597dbbf91d5cc3e5b9866d62a6780
201411096 <korea5781@gmail.com>
2021-02-04 11:56:55 +0900

    210204_02 sqlalchemy orm recursive query, func.row_number(), limit(), offset()

--- ex_05_flask_board/services/board/service_ver2.py
+++ ex_05_flask_board/services/board/service_ver2.py
@@ -73,17 +73,110 @@ def board_contentList(data):
     ;
     """
     
-	statement = text(query)
-	statement = statement.bindparams(
-		bindparam("startrow", type_=Integer), 
-		bindparam("numberInPage", type_=Integer),
-		bindparam("board_id", type_=String))
-	result = session.execute(statement, 
-		{"startrow":  data["startrow"], 
-		"numberInPage": data["numberInPage"],
-		"board_id":data['board_id']})
+    # ====================================== orm 이전 부분 ======================================
+	# statement = text(query)
+	# statement = statement.bindparams(
+	# 	bindparam("startrow", type_=Integer), 
+	# 	bindparam("numberInPage", type_=Integer),
+	# 	bindparam("board_id", type_=String))
+	# result = session.execute(statement, 
+	# 	{"startrow":  data["startrow"], 
+	# 	"numberInPage": data["numberInPage"],
+	# 	"board_id":data['board_id']})
 	
-	result = queryToDict2(result)
+	# result = queryToDict2(result)
+	# for row in result:
+	# 	for key in row.keys():
+	# 		if(type(row[key]) ==  datetime.datetime):
+	# 			row[key] = str(row[key])
+	# ====================================== orm 적용 부분 ======================================
+	result = None
+
+	initialRowSet = session.query(Board)\
+					.with_entities(
+						Board.board_content_id, Board.board_content_pid, Board.member_id,
+						Board.board_content_title, Board.board_content_body, 
+						Board.board_content_regdatetime, Board.board_content_edtdatetime,
+						Board.board_content_deleted,
+						literal(0, type_=Integer).label('depth'),
+						case([
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 0, func.concat('0000000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 1, func.concat('000000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 2, func.concat('00000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 3, func.concat('0000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 4, func.concat('000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 5, func.concat('00000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 6, func.concat('0000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 7, func.concat('000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 8, func.concat('00', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 9, func.concat('0', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 10, cast(Board.board_content_id, VARCHAR(1000)))
+						]).label('grp'),
+						case([
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 0, func.concat('0000000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 1, func.concat('000000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 2, func.concat('00000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 3, func.concat('0000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 4, func.concat('000000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 5, func.concat('00000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 6, func.concat('0000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 7, func.concat('000', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 8, func.concat('00', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 9, func.concat('0', cast(Board.board_content_id, VARCHAR(1000)))),
+							(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 10, cast(Board.board_content_id, VARCHAR(1000)))
+						]).label('lvl')
+					)\
+					.filter(and_(Board.board_content_pid == -1, Board.board_id == data['board_id'], Board.board_content_deleted == 'N') )\
+					.cte(recursive=True)
+	additionalRowSet = session.query(Board)\
+						.with_entities(
+							Board.board_content_id, Board.board_content_pid, Board.member_id,
+							Board.board_content_title, Board.board_content_body,
+							Board.board_content_regdatetime, Board.board_content_edtdatetime,
+							Board.board_content_deleted,
+							(initialRowSet.c.depth+1).label('depth'),
+							initialRowSet.c.grp.label('grp'),
+							func.concat(initialRowSet.c.lvl, '-', case([
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 0, func.concat('0000000000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 1, func.concat('000000000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 2, func.concat('00000000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 3, func.concat('0000000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 4, func.concat('000000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 5, func.concat('00000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 6, func.concat('0000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 7, func.concat('000', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 8, func.concat('00', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 9, func.concat('0', cast(Board.board_content_id, VARCHAR(1000)))),
+								(func.length(cast(Board.board_content_id, VARCHAR(1000))) == 10, cast(Board.board_content_id, VARCHAR(1000)))
+							])).label('lvl')
+						)\
+						.join(initialRowSet, Board.board_content_pid == initialRowSet.c.board_content_id)
+	recursiveRowSet = initialRowSet.union(additionalRowSet)
+
+	resultData = session.query(recursiveRowSet)\
+				.with_entities(
+					recursiveRowSet.c.board_content_id, recursiveRowSet.c.board_content_pid, recursiveRowSet.c.member_id,
+					recursiveRowSet.c.board_content_title, recursiveRowSet.c.board_content_body,
+					recursiveRowSet.c.board_content_regdatetime, recursiveRowSet.c.board_content_edtdatetime,
+					recursiveRowSet.c.board_content_deleted,
+					recursiveRowSet.c.depth,
+					recursiveRowSet.c.grp,
+					recursiveRowSet.c.lvl,
+					func.row_number().over(order_by=cast(recursiveRowSet.c.board_content_id, Integer)).label('board_content_num'),
+					Member.member_id,
+					Member.member_nickname,
+					Member.member_name
+				)\
+				.join(Member, Member.member_id == recursiveRowSet.c.member_id)\
+				.order_by(recursiveRowSet.c.grp.desc(), recursiveRowSet.c.lvl)\
+				.limit(data['numberInPage'])\
+				.offset(data['startrow'])
+	
+	result = queryToDict(resultData)
+	
+	for row in result:
+		print(row)
+
 	for row in result:
 		for key in row.keys():
 			if(type(row[key]) ==  datetime.datetime):
