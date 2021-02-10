68f14d18dc897354e093f8ee49e3b6820a675525
201411096 <korea5781@gmail.com>
2021-02-10 11:14:32 +0900

    210210_03

--- etc_ex/ex_26_cors/app_01/app.py
+++ etc_ex/ex_26_cors/app_01/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request, jsonify, render_template
+from flask import Flask, Response, request, jsonify, render_template, make_response, Response
 from flask_cors import CORS, cross_origin
 
 appHost = '192.168.0.51'
@@ -22,8 +22,20 @@ def test_b():
 
 @app.route('/c')
 @cross_origin(origins='http://192.168.0.51:3000')
+# @cross_origin(origins='http://192.168.0.51:30000') # 못받아옴
 def test_c():
     return {"c":"cc"}
+
+@app.route('/d')
+def test_d():
+    print('requestHeader : ', request.headers)                                  # requestHeader 출력
+
+    data = {"d":"dd"}
+    res = make_response(data)
+    # res.headers['Access-Control-Allow-Origin'] = '*'                          # 모든 origin 허용
+    res.headers['Access-Control-Allow-Origin'] = 'http://192.168.0.51:3000'     # ...
+    
+    return res
 
 if __name__ == '__main__':
     app.run(host=appHost, port=appPort, debug=True)
