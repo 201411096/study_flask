45ab1da15cadb507fa58506993a442301bd0b372
201411096 <korea5781@gmail.com>
2021-02-15 15:27:25 +0900

    210215_01

---
+++ etc_ex/ex_31_rabbitmq/send/send.py
@@ -0,0 +1,14 @@
+import pika
+
+# username과 password는 미리 설정했어야 함
+credentials = pika.PlainCredentials(username='abc', password='abc')
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.51', credentials=credentials))
+channel = connection.channel()
+
+queue_name = 'queue_name_01'
+channel.queue_declare(queue=queue_name)
+
+myMessage = 'abcabc ... ...'
+channel.basic_publish(exchange='', routing_key=queue_name, body=myMessage)
+
+connection.close()
