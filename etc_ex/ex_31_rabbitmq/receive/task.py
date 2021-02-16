98685784256dd7e0904f6f8d028593f2efc42417
201411096 <korea5781@gmail.com>
2021-02-16 10:34:09 +0900

    210216_01

--- etc_ex/ex_31_rabbitmq/receive/task.py
+++ etc_ex/ex_31_rabbitmq/receive/task.py
@@ -4,12 +4,18 @@ import time
 # def callback(ch, method, properties, body):
 #     print(" [x] Received %r" % body)
 
-def task1(channel, method, properties, body):
-    # print('check arguemnts ...')
-    # print(channel)
-    # print(method)
-    # print(properties)
+def task1(ch, method, properties, body):
+    print('check arguemnts ...')
+    print(ch)
+    print(method)
+    print(properties)
+    # 단순 메시지 테스트
     # print(body)
+    # dictionary 테스트
+    import json
+    body = json.loads(body)
+    print(body['a'])
+    
     num = random.randrange(1, 1000000)
     print(num)
     time.sleep(num/100000)
--- etc_ex/ex_31_rabbitmq/send/send.py
+++ etc_ex/ex_31_rabbitmq/send/send.py
@@ -1,5 +1,5 @@
 import pika
-
+import json
 # username과 password는 미리 설정했어야 함
 credentials = pika.PlainCredentials(username='abc', password='abc')
 connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.51', credentials=credentials))
@@ -8,7 +8,11 @@ channel = connection.channel()
 queue_name = 'queue_name_01'
 channel.queue_declare(queue=queue_name)
 
-myMessage = 'abcabc ... ...'
+# myMessage = 'abcabc ... ...'
+# channel.basic_publish(exchange='', routing_key=queue_name, body=myMessage)
+
+myMessage = {"a":"aa", "b":"bb"}
+myMessage = json.dumps(myMessage)
 channel.basic_publish(exchange='', routing_key=queue_name, body=myMessage)
 
 connection.close()
