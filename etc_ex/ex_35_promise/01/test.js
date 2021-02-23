77f32b1ef1c563be9bf80c277ee7bac9ab486186
201411096 <korea5781@gmail.com>
2021-02-23 11:10:31 +0900

    210223_01

---
+++ etc_ex/ex_35_promise/01/test.js
@@ -0,0 +1,17 @@
+function divide(numA, numB){
+    return new Promise( (resolve, reject) =>{
+        if(numB === 0){
+            reject(new Error("unable to divide by 0."));
+        }else{
+            resolve(numA / numB)
+        }
+    });
+}
+
+divide(8, 2)
+.then( (result) => {console.log("result(success) : ", result)} )
+.catch( (error) => {console.log("result(error) : ", error)} )
+
+divide(8, 0)
+.then( (result) => {console.log("result(success) : ", result)} )
+.catch( (error) => {console.log("result(error) : ", error)} )
